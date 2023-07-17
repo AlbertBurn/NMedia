0c39ff387439ea1035d82a15356c00c5da17e68f
Albert R. Burnyashov <albrburn@gmail.com>
2023-07-17 12:56:02 +0300

    Task andad 1.3 Arch2 attempt2

--- app/src/main/java/ru/netology/nmedia/db/AppDb.kt
+++ app/src/main/java/ru/netology/nmedia/db/AppDb.kt
@@ -4,8 +4,10 @@ package ru.netology.nmedia.db
 import androidx.room.*
 import ru.netology.nmedia.dao.Converters
 import ru.netology.nmedia.dao.PostDao
+import ru.netology.nmedia.dao.PostRemoteKeyDao
 import ru.netology.nmedia.dto.AttachmentType
 import ru.netology.nmedia.entity.PostEntity
+import ru.netology.nmedia.entity.PostRemoteKeyEntity
 
 class Converters {
     @TypeConverter
@@ -15,9 +17,9 @@ class Converters {
     fun toAttachmentType(value: String) = enumValueOf<AttachmentType>(value)
 }
 
-@Database(entities = [PostEntity::class], version = 1, exportSchema = false)
+@Database(entities = [PostEntity::class, PostRemoteKeyEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDb : RoomDatabase() {
     abstract fun postDao(): PostDao
-
+    abstract fun postRemoteKeyDao(): PostRemoteKeyDao
 }
